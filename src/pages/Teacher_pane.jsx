4a2b8f0b26a99d01d3760bc7a6047b26bdf9b0a5
Akbarof01 <akhbbarov01@gmail.com>
2024-03-26 20:41:00 +0500

    Teacher panel

--- src/pages/Teacher_pane.jsx
+++ src/pages/Teacher_pane.jsx
@@ -3,6 +3,10 @@ function Teacher_pane() {
         <>
         <div className="head">
             <div className='container'>
+            <div className="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 16 16"><g fill="#0a81ff"><path d="M11 6a3 3 0 1 1-6 0a3 3 0 0 1 6 0"/><path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/></g></svg>
+
+                    </div>
                 <div className='Tech_Panel'>
                     <h5 className='Techer'>Akbarov Abdulaziz</h5>
                     <h5 className='Soxa'>Frontend react
