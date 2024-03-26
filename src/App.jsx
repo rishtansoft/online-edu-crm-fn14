4bff175f1e67c72511ac5a1c8d0bbc2d42cfdcfd
qosimov007 <mirjalolq274@gmail.com>
2024-03-26 16:22:57 +0200

    Student admin panel

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,177 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-
-
-
-
-
-
-
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+<div className="navbar">
+  
+<div className="student">
+        <h1>O‘quvchilar</h1>
+        <p>O‘quvchilarni ko‘rishingiz mumkin</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="input-grup">
+        <input type="text"  placeholder='Qidiruv o‘quvchilar, guruhlar va boshqa...'  />
+        <img  width={44} height={44}  src="src/img/Group 5.png" alt=""/>
+        <img width={44} height={44}  src="src/img/Ellipse 2.png" alt=""/>
+        <div className="text">
+          <h1>Sarvarbek</h1>
+          <p>CEO</p>
+        </div>
+        <img className='freme'  width={18} height={18} src="src/img/Frame.png" alt=""/>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+</div>
+<hr />
+
+<div className="header">
+<input className='qidiruv' type="text" placeholder='Qidiruv' />
+
+<div className="button-grup">
+  <button className='Xabar'>Xabar</button>
+  <button className='Export'>Export</button>
+</div>
+</div>
+
+
+
+<table class="table">
+  <thead>
+    <tr  >
+      <th  className='th' scope="col"><input type="checkbox"  /> ID</th>
+      <th scope="col">Ism Familiya Otasining ismi</th>
+      <th scope="col">Guruhi</th>
+      <th scope="col">Ustozi</th>
+      <th scope="col">Telefon raqami</th>
+      <th scope="col">To'lovi</th>
+      <th scope="col"></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row"><input type="checkbox"  />100</th>
+      <td><a href="#">Durdona Egamberdiyeva</a></td>
+      <td>Pre intermediate</td>
+      <td>Durdona Xudoberganova</td>
+      <th scope="col"> +998933322222</th>
+      <th scope="col"><button className='tolov'>To‘lov</button></th>
+      <th scope="col"><img src="src/img/Frame (1).png" alt="" /></th>
+    </tr>
+    <tr>
+      <th scope="row"><input type="checkbox"  />102</th>
+      <td><a href="#">Durdona Egamberdiyeva</a></td>
+      <td>Pre intermediate</td>
+      <td>Durdona Xudoberganova</td>
+      <th scope="col">+998933322222</th>
+      <th scope="col"><button className='tolov'>To‘lov</button></th>
+      <th scope="col"><img src="src/img/Frame (1).png" alt="" /></th>
+    </tr>
+    <tr>
+      <th scope="row"><input type="checkbox"  />105</th>
+      <td><a href="#">Durdona Egamberdiyeva</a></td>
+      <td>Beginner</td>
+      <td>Durdona Xudoberganova</td>
+      <th scope="col">+998933322222</th>
+      <th scope="col"><button className='tolov'>To‘lov</button></th>
+      <th scope="col"><img src="src/img/Frame (1).png" alt="" /></th>
+    </tr>
+    <tr>
+      <th scope="row"><input type="checkbox"  />105</th>
+      <td><a href="#">Durdona Egamberdiyeva</a></td>
+      <td>Beginner</td>
+      <td>Durdona Xudoberganova</td>
+      <th scope="col">+998933322222</th>
+      <th scope="col"><button className='tolov'>To‘lov</button></th>
+      <th scope="col"><img src="src/img/Frame (1).png" alt="" /></th>
+    </tr>
+    <tr>
+      <th scope="row"><input type="checkbox"  />102</th>
+      <td><a href="#">Durdona Egamberdiyeva</a></td>
+      <td>Pre intermediate</td>
+      <td>Durdona Xudoberganova</td>
+      <th scope="col">+998933322222</th>
+      <th scope="col"><button className='tolov'>To‘lov</button></th>
+      <th scope="col"><img src="src/img/Frame (1).png" alt="" /></th>
+    </tr>
+    <tr>
+      <th scope="row"><input type="checkbox"  />105</th>
+      <td><a href="#">Durdona Egamberdiyeva</a></td>
+      <td>Beginner</td>
+      <td>Durdona Xudoberganova</td>
+      <th scope="col">+998933322222</th>
+      <th scope="col"><button className='tolov'>To‘lov</button></th>
+      <th scope="col"><img src="src/img/Frame (1).png" alt="" /></th>
+    </tr>
+    <tr>
+      <th scope="row"><input type="checkbox"  />105</th>
+      <td><a href="#">Durdona Egamberdiyeva</a></td>
+      <td>Beginner</td>
+      <td>Durdona Xudoberganova</td>
+      <th scope="col">+998933322222</th>
+      <th scope="col"><button className='tolov'>To‘lov</button></th>
+      <th scope="col"><img src="src/img/Frame (1).png" alt="" /></th>
+    </tr>
+    <tr>
+      <th scope="row"><input type="checkbox"  />102</th>
+      <td><a href="#">Durdona Egamberdiyeva</a></td>
+      <td>Pre intermediate</td>
+      <td>Durdona Xudoberganova</td>
+      <th scope="col">+998933322222</th>
+      <th scope="col"><button className='tolov'>To‘lov</button></th>
+      <th scope="col"><img src="src/img/Frame (1).png" alt="" /></th>
+    </tr>
+    <tr>
+      <th scope="row"><input type="checkbox"  />105</th>
+      <td><a href="#">Durdona Egamberdiyeva</a></td>
+      <td>Beginner</td>
+      <td>Durdona Xudoberganova</td>
+      <th scope="col">+998933322222</th>
+      <th scope="col"><button className='tolov'>To‘lov</button></th>
+      <th scope="col"><img src="src/img/Frame (1).png" alt="" /></th>
+    </tr>
+    <tr>
+      <th scope="row"><input type="checkbox"  />105</th>
+      <td><a href="#">Durdona Egamberdiyeva</a></td>
+      <td>Beginner</td>
+      <td>Durdona Xudoberganova</td>
+      <th scope="col">+998933322222</th>
+      <th scope="col"><button className='tolov'>To‘lov</button></th>
+      <th scope="col"><img src="src/img/Frame (1).png" alt="" /></th>
+    </tr>
+    
+  </tbody>
+
+
+
+
+</table>
+
+<nav aria-label="Page navigation example">
+  <ul class="pagination">
+    <li class="page-item">
+      <a class="page-link" href="#" aria-label="Previous">
+        <span aria-hidden="true">&laquo;</span>
+      </a>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item">
+      <a class="page-link" href="#" aria-label="Next">
+        <span aria-hidden="true">&raquo;</span>
+      </a>
+    </li>
+  </ul>
+</nav>
+
+
+<div className="btn-grup">
+  <button className='btn1'>To‘lov qilganlar -100</button>
+  <button className='btn2' >To‘lov qilmaganlar -15</button>
+  <button className='btn3' >Qarzdorlar -100</button>
+</div>
+
     </>
   )
 }
